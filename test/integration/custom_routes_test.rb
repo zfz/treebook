07f986293bcbb5fc20c72557554a43a7aaf92371
zfz <zhngfz@gmail.com>
2014-05-01 04:25:56 -0400

    Updated profile and added scoped status updates to it

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -17,4 +17,8 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "that a profile page works" do
+    get '/zfz'
+    assert_response :success
+  end
 end
