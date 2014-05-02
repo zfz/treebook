74867bd582633a540ba5856a5fa83be1bb2ec17f
zfz <zhngfz@gmail.com>
2014-05-02 10:29:45 -0400

    Change application.rb to make styling work on heroku

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,7 @@ module Treebook
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
+
+    config.assets.compile = true
   end
 end
